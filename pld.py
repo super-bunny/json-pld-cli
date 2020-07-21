0ebfdc49066c30248bc4ddbbc398df3bff95ec1a
Clovis de Villars <clovis.devillars@gmail.com>
2020-07-21 15:44:22 +0200

    Add user story verbose print in total estimated duration calculation function

--- pld.py
+++ pld.py
@@ -1,4 +1,7 @@
-def total_estimated_duration(pld_content) -> int:
+import pld_messages
+
+
+def total_estimated_work_duration(pld_content, verbose_print=False) -> int:
     total_duration = 0
     if 'deliverables' in pld_content:
         for deliverables in pld_content['deliverables']:
@@ -6,5 +9,8 @@ def total_estimated_duration(pld_content) -> int:
                 for subset in deliverables['subsets']:
                     if 'userStories' in subset:
                         for user_story in subset['userStories']:
+                            if verbose_print:
+                                print(f'{pld_messages.estimated_duration_message(user_story["estimatedDuration"])} '
+                                      f'{user_story["name"]}')
                             total_duration += user_story['estimatedDuration']
     return total_duration
