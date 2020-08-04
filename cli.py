0753527463f953d3e0ac01a480116213671c3d41
Clovis de Villars <clovis.devillars@gmail.com>
2020-08-04 16:18:52 +0200

    Add "assignees" command that print user stories of one given user

--- cli.py
+++ cli.py
@@ -24,6 +24,7 @@ def read_file(file_path) -> str:
 COMMAND_TABLE = {
     'duration': lambda arguments: commands.duration_cmd(pld_content, arguments.get('verbose', False)),
     'distribution': lambda arguments: commands.distribution_cmd(pld_content),
+    'assignees': lambda arguments: commands.assignees_cmd(pld_content, arguments.get('user')),
     'test': lambda arguments: print(arguments)
 }
 
@@ -39,6 +40,8 @@ parser_duration = subparsers.add_parser('duration')
 parser_duration.add_argument('-v', type=bool, dest='verbose', const=True, default=False, nargs='?',
                              help='print estimated duration for each user story')
 parser_repartition = subparsers.add_parser('distribution')
+parser_assignees = subparsers.add_parser('assignees')
+parser_assignees.add_argument('user', type=str, default=None, help='user filter for assignees command')
 parser_duration = subparsers.add_parser('test')
 
 args = parser.parse_args()
